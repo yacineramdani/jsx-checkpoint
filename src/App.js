f2f96f061432bf0f138f0175c019ed62d21df04b
yacineramdani <yacine1001@hotmail.com>
2021-09-10 11:10:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import imageSrc from"./cookies.jpg"
+import myVideo from "./GOMYCODE HACKS HACKATHON.mp4"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 class="title red">Yacine Ramdani</h1>
+        <br />
+        <img src={imageSrc} alt="" />
+        <br />
+        <img src="/husky.jfif" alt="" />
+      </div>
+      <video width="320" height="240" controls >
+        <source src={myVideo}type="video/mp4"/>
+      </video>
     </div>
   );
 }
